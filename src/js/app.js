584a5dcc66563a9fb3dfb00f98fca4073797223e
Yose Apero <yose15ti@mahasiswa.pcr.ac.id>
2020-12-23 23:27:49 +0700

    membuat app remember login status

--- src/js/app.js
+++ src/js/app.js
@@ -17,16 +17,30 @@ import '../css/app.css';
 // Import App Component
 import App from '../components/app.vue';
 import store from "../pages/store/store"
+import firebase from 'firebase';
 // Init Framework7-Vue Plugin
 Framework7.use(Framework7Vue);
 
-// Init App
-new Vue({
-    el: '#app',
-    render: (h) => h(App),
-    store,
-    // Register App Component
-    components: {
-        app: App
-    },
-});
+let newapp = null
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      store.commit('setSignedIn', true)
+    } else {
+      // No user is signed in.
+      store.commit('setSignedIn', false)
+    }
+
+    if(!newapp){
+        newapp = // Init App
+        new Vue({
+            el: '#app',
+            render: (h) => h(App),
+            store,
+            // Register App Component
+            components: {
+                app: App
+            },
+        });
+    }
+  });
