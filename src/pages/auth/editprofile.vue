c7dc36a0b159109e1361ead824ea28d2514b78cd
Yose Apero <yose15ti@mahasiswa.pcr.ac.id>
2021-01-09 20:57:17 +0700

    Modify the data and ui inside edit profile page

--- src/pages/auth/editprofile.vue
+++ src/pages/auth/editprofile.vue
@@ -11,40 +11,14 @@
     </div>
 
     <f7-list no-hairlines-md>
-      <f7-list-input
-        :value="name"
-        @input="name = $event.target.value"
-        label="Name"
-        type="text"
-        placeholder="Your name"
-        clear-button
-      >
+      <f7-list-input :value="display_name" @input="display_name = $event.target.value" label="Name" type="text" placeholder="Your name" clear-button >
+      
       </f7-list-input>
 
-      <f7-list-input
-        :value="email"
-        @input="email = $event.target.value"
-        label="E-mail"
-        type="email"
-        placeholder="Your e-mail"
-        clear-button
-      >
-      </f7-list-input>
-
-      <f7-list-input
-        :value="password"
-        @input="password = $event.target.value"
-        label="Password"
-        type="password"
-        placeholder="Your password"
-        clear-button
-        
-      >
-      </f7-list-input>
     </f7-list>
 
     <f7-block>
-      <f7-button fill round @click="signUp">Submit</f7-button>
+      <f7-button fill round >Update</f7-button>
       <input
         type="file"
         ref="file"
@@ -70,22 +44,27 @@ export default {
     };
   },
   computed: {
+
+    display_name:{
+        get:function(){
+            return this.$store.getters.display_name
+        },
+        set:function(newValue){
+            this.$store.commit('setDisplayName', newValue)
+        }
+    },
     image_url() {
       return this.$store.getters.image_url
     },
     files() {
       return this.$store.getters.files
     },
-    signed_up() {
-      return this.$store.getters.signed_up
+    photo_url(){
+        return this.$store.getters.photo_url
     }
   },
   watch: {
-    signed_up(value) {
-      if (value == true) {
-        this.$f7router.navigate('/signin/');
-      }
-    }
+    
   },
 
   methods: {
@@ -96,25 +75,12 @@ export default {
       //read the image file
       this.$store.dispatch("readFile");
     },
-    signUp() {
-      const self = this;
-      var payload = {};
-      payload.name = this.name;
-      payload.email = this.email;
-      payload.password = this.password;
-      payload.photoURL = this.image_url;
-      if (self.files) {
-        self.$store.dispatch("uploadFile").then((url) => {
-          payload.photoURL = url;
-          self.$store.dispatch("signUp", payload);
-        });
-      } else {
-        this.$store.dispatch("signUp", payload);
-      }
-          },
+    
   },
   created() {
-    this.$store.commit('setSignedUp', false)
+    if(this.photo_url!= null){
+        this.$store.commit('setImageURL',this.photo_url)
+    }
   }
 };
 </script>
